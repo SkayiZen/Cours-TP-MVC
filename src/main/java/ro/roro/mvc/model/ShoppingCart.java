6b4b0d5ccd50ef313ed3cf27cd82a567c8cbe035
Roro <skayizen@gmail.com>
2026-01-29 20:33:22 +0100

    🚧 Implement ShoppingCart with items management

--- src/main/java/ro/roro/mvc/model/ShoppingCart.java
+++ src/main/java/ro/roro/mvc/model/ShoppingCart.java
@@ -1,4 +1,30 @@
 package ro.roro.mvc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    private List<Product> items = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        items.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        items.remove(product);
+    }
+
+    public List<Product> getItems() {
+        return items;
+    }
+
+    public double getTotalPrice() {
+        return items.stream().mapToDouble(Product::getFinalPrice).sum();
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
 }
