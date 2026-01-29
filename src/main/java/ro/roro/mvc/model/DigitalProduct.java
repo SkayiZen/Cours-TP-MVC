7b722cb32a8881dbeb3a097f46fba085934dccac
Roro <skayizen@gmail.com>
2026-01-29 20:32:42 +0100

    🚧 Add DigitalProduct model

--- src/main/java/ro/roro/mvc/model/DigitalProduct.java
+++ src/main/java/ro/roro/mvc/model/DigitalProduct.java
@@ -1,4 +1,54 @@
 package ro.roro.mvc.model;
 
-public class DigitalProduct {
+public class DigitalProduct implements Product {
+
+    private String name;
+    private double initPrice;
+    private double finalPrice;
+    private String licenseType;
+
+    public DigitalProduct(String name, double price, String licenseType) {
+        this.name = name;
+        this.initPrice = price;
+        this.finalPrice = price;
+        this.licenseType = licenseType;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return initPrice;
+    }
+
+    @Override
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    @Override
+    public int getQuantityStock() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void reduceStock(int quantity) {
+
+    }
+
+    @Override
+    public void applyDiscount(double percentage) {
+        if(percentage >= 0 && percentage <= 100) {
+            finalPrice = initPrice * (1 - percentage / 100);
+        }
+    }
+
+    @Override
+    public String getType() {
+        return "Digital";
+    }
 }
