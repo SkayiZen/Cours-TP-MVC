ea96ddc2db02040c454570453e1579c93ae484c3
Roro <skayizen@gmail.com>
2026-01-29 20:31:30 +0100

    🚧 Main entry point

--- src/main/java/ro/roro/mvc/Main.java
+++ src/main/java/ro/roro/mvc/Main.java
@@ -1,4 +1,13 @@
 package ro.roro.mvc;
 
+import ro.roro.mvc.controller.ShopController;
+import ro.roro.mvc.view.ShopView;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ShopController controller = new ShopController(new ShopView());
+        controller.run();
+    }
+
 }
