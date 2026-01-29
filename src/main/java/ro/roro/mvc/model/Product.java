e9d7aaa75007cc0462003532c022cbe27c2648c7
Roro <skayizen@gmail.com>
2026-01-29 20:32:08 +0100

    🚧 AAdd Product interface

--- src/main/java/ro/roro/mvc/model/Product.java
+++ src/main/java/ro/roro/mvc/model/Product.java
@@ -1,4 +1,13 @@
 package ro.roro.mvc.model;
 
-public class Product {
+public interface Product {
+
+        String getName();
+        double getPrice();
+        double getFinalPrice();
+        int getQuantityStock();
+        void reduceStock(int quantity);
+        void applyDiscount(double percentage);
+        String getType();
+
 }
